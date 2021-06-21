f6de62815c09e3f6145e29cbf47e35f19fcf008e
Melvin Baybayanon <melvsbaybayanon@gmail.com>
2021-06-21 18:53:51 +0800

    Fix toggle Main na

--- src/components/Footer/FooterNav.js
+++ src/components/Footer/FooterNav.js
@@ -1,17 +1,35 @@
-export const FooterNav = ({title, nav}) => {
- let menuTab = []
- nav.forEach(e => {
-     menuTab.push(<li key={e}>{e}</li>)
- })
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus, faMinus } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
+
+export const FooterNav = ({ title, nav }) => {
+  let [toggle, setToggle] = useState(false);
+  const showToggleHandler = (e) => {
+    if (
+      e.target.className.split(" ").indexOf("footer__middle--site-map") > -1
+    ) {
+      e.target.querySelector("ul").classList.toggle("footer__nav--show");
+      setToggle(!toggle)
+    }
+  };
+
+  let menuTab = [];
+  nav.forEach((e) => {
+    menuTab.push(<li key={e}>{e}</li>);
+  });
 
   return (
-    <div className="footer__middle--site-map">
+    <div
+      onClick={(e) => showToggleHandler(e)}
+      className="footer__middle--site-map"
+    >
       <h5 className="footer--heading">
-        {title} <i className="fas fa-arrow-up"></i>
+        {title}
+        <i>
+          <FontAwesomeIcon icon={toggle ? faMinus : faPlus} />
+        </i>
       </h5>
-      <ul>
-        {menuTab}
-      </ul>
+      <ul>{menuTab}</ul>
     </div>
   );
 };
